3dfce0aaf36debc5965dc7be58c627a374b8c873
yotamos5699 <m.lhafakot@gmail.com>
2022-11-20 18:03:10 +0200

    -updated packege json

--- src/castumReports.js
+++ src/castumReports.js
@@ -2,12 +2,7 @@
 //Must to configure private data on apiConfig.js file
 const wizlib = require("wizcloud-api");
 const fs = require("fs");
-const myObjData = require("../src/Helpers/wizCloudUtiles/helpers/apiConfig.json");
-//var express = require("express");
-//var router = express.Router();
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Api methods
+const myObjData = require("./apiConfig.json");
 
 async function exportCastumersRecords(fileData) {
   //console.log("filedata" + fileData);
@@ -17,13 +12,6 @@ async function exportCastumersRecords(fileData) {
   var myDBname = await myObjData[0].WizcloudApiDBName;
   wizlib.init(myObjData[0].WizcloudApiPrivateKey, myObjData[0].WizcloudApiServer);
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  //Encrypted report data from "External" option in report issue
-  //console.log(myObjData.stockEncrypt_reportData);
-  //encrypt_reportData = myObjData.castumersEncryptData;
-  //params_data = myObjData.params_data_ca;
-
   let { reportCod, parameters } = await fileData;
 
   let apiRes = await wizlib.exportDataRecords(myDBname, {
@@ -31,21 +19,10 @@ async function exportCastumersRecords(fileData) {
     parameters: parameters,
   });
   const jsondata = JSON.parse(apiRes);
-  // console.log(jsondata);
+
   const data = JSON.stringify(jsondata, null, 2);
-  //console.log(data);
 
   return data;
 }
 
 module.exports.exportCastumersRecords = exportCastumersRecords;
-//exportCastumersRecords();
-/*router.get("/", async function (req, res) {
-  let apiRes = await wizlib.exportDataRecords(myDBname, {
-    datafile: encrypt_reportData,
-    parameters: params_data,
-  });
-  res.json(apiRes);
-});
-
-module.exports = router; */
